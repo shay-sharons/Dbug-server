b00751d47f63444cb8686322e5e8daece7cd1937
SUPERita <itamarhefetzsahar@gmail.com>
2022-07-12 15:31:38 +0300

    1

--- index.js
+++ index.js
@@ -23,8 +23,9 @@ app.get('/', (req, res) => {
 const CONNECTION_URL = process.env.CONNECTION_URL; 
 const PORT = process.env.PORT || 5000;
 
+/*
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=> app.listen(PORT, ()=> console.log(`server running on port ${PORT}`)))
 .catch ((error)=> console.log(error.message));
-
+*/
 //mongoose.set('useFindAndModify', false);
